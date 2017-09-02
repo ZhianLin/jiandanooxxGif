44718e3b4c0c73e9758dd0eecf348988677f9e8d
lza <564672836@qq.com>
2017-09-02 14:58:50 +0800

    去掉内存数据库的插入
    把数据库插入语句去掉

--- ooxx.py
+++ ooxx.py
@@ -11,30 +11,7 @@ import sys,time,socket,os
 import sqlite3,md5
 
 def dataInsert(path,insertStr,insertData):
-#execute执行SQL脚本，参数放在元组中，用?做占位符
-#示例格式
-#insertData=(None,201,'s5731.jpg','001','http://s.umei.cc/small/files/s5731.jpg','http://s.umei.cc/small/files/s5731.jpg','tab11')
-#insertStr='insert into DownloadHistory(pid,filename,filemd5,fileurl,openPageUrl,tag)values(?,?,?,?,?,?)'
-#cur.execute(insertStr,insertData)
-    if not insertStr or insertData==[()]:return 0
-    #打开文件数据库
-    con_file=sqlite3.connect(path)
-    cur_file=con_file.cursor()
-    #把内存数据库的值插入到文件数据库
-    
-    for i in xrange(len(insertData)):
-        print u'插入第%d行数据'%i
-        #insertData=
-        cur_file.execute(insertStr,insertData[i])
-    #提交SQL命令
-    con_file.commit()
-#    cur_file.execute('select * from DownloadHistory')
-#    print u'文件数据库内容',cur_file.fetchall()
-    cur_file.close()
 
-#打开网页
-q=Queue.Queue(maxsize=200)#网页中的文件队列
-f=Queue.Queue(20)#等待下载的队列
 
 def getHtml(PageUrl):
     req_header = {'User-Agent':'Mozilla/6.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'}
@@ -84,7 +61,7 @@ def getImage(path,HTMLTempStr):
     if not imageUrl:
         print u'没有获取到图片地址'
         sys.exit()
-    insertData=[]
+#    insertData=[]
     for match in imageUrl:
         URL=match.group()
         filename=URL.split('/')[-1]
@@ -101,21 +78,17 @@ def downImage(DownList):##DownList=[URL,fullname,filename]
         #文件下载
         try:
             cont=urllib2.urlopen(URL,timeout=13).read()
-    #            datlist=(None,filename,md5(cont),path,URL,None,'ooxx')
-    #            insertData.append(datlist)
             f=open(fullname,'wb')
             f.write(cont)
             f.close()
             global imageDownloadCount
+            global DownRound
             imageDownloadCount=imageDownloadCount+1
             print u'第 %d 页,第 %d 个图像下载完成'%(DownRound,imageDownloadCount)
             print u'第 %d 页,共下载 %d 个图像'%(DownRound,imageDownloadCount-tNum)
         except Exception,e:
             print e
-    #插入数据库记录
-#    insertStr='insert into DownloadHistory(pid,filename,filemd5,fileurl,openPageUrl,tag)values(?,?,?,?,?,?)'
-#    if insertData<>[()]:dataInsert(u'E:/Python Project/DownloadHistory.db3',insertStr,insertData)
-    #下载下一页图片
+     #下载下一页图片
     global DownRound
     DownRound=DownRound+1
     if DownRound<=12:
@@ -126,7 +99,7 @@ def downImage(DownList):##DownList=[URL,fullname,filename]
 #图片保存目录
 save_Path=u'E:/Girls/'
 #开始的网址
-page=1880
+page=1
 #startPage='http://jiandan.net/ooxx/page-1569#comments'
 #startPage='http://jiandan.net/ooxx/page-1558#comments'
 #startPage='http://jiandan.net/ooxx/page-1534#comments'
